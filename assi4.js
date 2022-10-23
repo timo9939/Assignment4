cd7f376cc82c39302e8719ff2ddb24973b436d24
timo9939 <timothylau20199@gmail.com>
2022-10-23 16:57:22 +0100

    timer added & go back button

--- assi4.js
+++ assi4.js
@@ -1,18 +1,28 @@
-
+//definition of the buttons and content
 const startButton =document.getElementById('start-btn');
 const nextButton=document.getElementById('next-btn');
 const submitButton=document.getElementById('submit');
+const goBackButton=document.getElementById('go-back');
+let submit_description= document.getElementById('submit-discription');
+let high_scores_description=document.getElementById('high-scores-description');
+let initial_input=document.getElementById('initial-input');
 
+//get Element from html
 const bigTitle=document.getElementById('big-header');
 const description=document.getElementById('descritption');
 let displayQuestion=document.getElementById('question');
 
+//get element from the buttons
 let buttonA= document.getElementById('btnA');
 let buttonB= document.getElementById('btnB');
 let buttonC= document.getElementById('btnC');
 let buttonD= document.getElementById('btnD');
 
+//Define the score and get element from timer
 let score=0;
+timerDisplay=document.getElementById('time-display');
+
+//Define question number
 let question_num=0;
 let ans_a=0;
 let ans_b=1;
@@ -20,27 +30,46 @@ let ans_c=2;
 let ans_d=3;
 let next_button_times=1; 
 
-let submit_description= document.getElementById('submit-discription');
-let high_scores_description=document.getElementById('high-scores-description');
-
+//Setting reaction for the button
 startButton.addEventListener('click',startQuestion);
 nextButton.addEventListener('click',nextQuestion);
+submitButton.addEventListener('click',afterSubmit);
+goBackButton.addEventListener('click',nextQuestion);
+buttonA.addEventListener('click',nextQuestion);
+buttonB.addEventListener('click',nextQuestion);
+buttonC.addEventListener('click',nextQuestion);
+buttonD.addEventListener('click',nextQuestion);
 
-
-
+//Question 
 question=['Commonly used data types Do not include:',
 'The condition in an if/else statement is enclosed with______.', 
 'Arrays in JavaScript can be used to store_____.',
 'String values must be enclosed within _____ when being assigned to variables.',
 'A very useful tool used during development and debugging for printing content to the debugger is:'];
 
+//Answer
 Answer=['strings','booleans','alerts','numbers',
 'quotes','curly brackets','parenthesis','square brackets',
 'numbers and strings','other arrays','booleans','all of the above',
 'commas','curly brackets','quotes','parenthesis',
 'JavaScript','terminal/bash','for loops','console.log']
 
+//Function of the timer
+function countdown(){
 
+    var timeLeft=90;
+    timer2 =setInterval(function(){
+        if(timeLeft>1){
+           timerDisplay.textContent='Time: '+ timeLeft;
+           timeLeft--;
+           
+        }
+    },1000);
+
+
+}
+
+//Function of the start button
 function startQuestion(){
     console.log('Game Started');
     startButton.classList.add('hide');
@@ -54,17 +83,21 @@ function startQuestion(){
     nextButton.classList.remove('hide');
 
     
-    document.getElementById('question').textContent=question[0]
-    document.getElementById('btnA').textContent=Answer[0]
-    document.getElementById('btnB').textContent=Answer[1]
-    document.getElementById('btnC').textContent=Answer[2]
-    document.getElementById('btnD').textContent=Answer[3]
+    document.getElementById('question').textContent=question[0];
+    document.getElementById('btnA').textContent=Answer[0];
+    document.getElementById('btnB').textContent=Answer[1];
+    document.getElementById('btnC').textContent=Answer[2];
+    document.getElementById('btnD').textContent=Answer[3];
+
+    countdown();
+   
 }
 
+//Function of the next button
 function nextQuestion(){
     
     
-    if(next_button_times<=5){
+    if(next_button_times<5){
     
     next_button_times++;
     console.log("Clicking Time of the next_button is"+next_button_times);
@@ -90,18 +123,22 @@ function nextQuestion(){
     buttonB.classList.add('hide');
     buttonC.classList.add('hide');
     buttonD.classList.add('hide');
-    
+
     submit_description.classList.remove('hide');
+    clearInterval(timer2);
+    timerDisplay.classList.add('hide');
 
 
     }
 }
 
-submitButton.addEventListener('click',afterSubmit);
-
+//Function of the submit button
 function afterSubmit(){
     submit_description.classList.add('hide');
     high_scores_description.classList.remove('hide');
+    document.getElementById('high-scores').textContent=document.getElementById('initial-input').value;
+
+    localStorage.setItem("initial_input",score);
 }
 
 
